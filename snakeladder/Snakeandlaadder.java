42e5174dc4a958ac03d3b87c6bed53e83d543f49
suhantsakunde07 <sushantsakunde07@gmail.com>
2021-12-31 22:15:08 +0530

    Aded: Player checks position

--- snakeladder/Snakeandlaadder.java
+++ snakeladder/Snakeandlaadder.java
@@ -1,9 +1,27 @@
 package snakeladder;
 public class Snakeandlaadder {
-public static void main(String[] args) {
-	       int START_POSITION=0;
-	       System.out.println("Player is at start position  "+START_POSITION);
-	       int rolldice=(int)(Math.floor(Math.random()*6+1));
-	       System.out.println("After rollig dice Player gets number: "+rolldice);      
-}	
+	public static final int NoPlayCase=0;
+	public static final int LadderCase=1;
+	public static final int SnakeCase=2;
+	public static void main(String[] args) {
+		double position =0;
+		double dicerolls= (Math.floor(Math.random()*6+1));
+		System.out.println("Number got by dice rolling "+dicerolls);
+		int option = (int)Math.floor(Math.random()*3);
+		switch (option) {
+		case NoPlayCase:
+			position= position + 0;
+			break;
+		case LadderCase:
+			position=position+ dicerolls;
+			break;
+		case SnakeCase:
+			position=position- dicerolls;
+			break;
+		    }
+		if (position<0) {
+			position=0;
+		    }
+		System.out.println("Player moved to position: "+position);
+}				
 }
