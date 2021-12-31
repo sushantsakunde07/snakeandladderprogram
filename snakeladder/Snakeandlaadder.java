54bd9253930d49e60d3a899da1e3ea44476a3d67
suhantsakunde07 <sushantsakunde07@gmail.com>
2021-12-31 22:00:57 +0530

    Added player's start position

---
+++ snakeladder/Snakeandlaadder.java
@@ -0,0 +1,8 @@
+package snakeladder;
+public class Snakeandlaadder {
+	public static void main(String[] args) {
+		int PLAYER1=0;
+		int START_POSITION=0;
+		System.out.println("Player is at start position  "+START_POSITION);	
+}
+}
