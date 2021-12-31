b1a1f8b94f6c11ba83baef0f48cb40746abe30d4
suhantsakunde07 <sushantsakunde07@gmail.com>
2021-12-31 22:11:24 +0530

    Added: player rolls die

--- snakeladder/Snakeandlaadder.java
+++ snakeladder/Snakeandlaadder.java
@@ -1,8 +1,9 @@
 package snakeladder;
 public class Snakeandlaadder {
-	public static void main(String[] args) {
-		int PLAYER1=0;
-		int START_POSITION=0;
-		System.out.println("Player is at start position  "+START_POSITION);	
-}
+public static void main(String[] args) {
+	       int START_POSITION=0;
+	       System.out.println("Player is at start position  "+START_POSITION);
+	       int rolldice=(int)(Math.floor(Math.random()*6+1));
+	       System.out.println("After rollig dice Player gets number: "+rolldice);      
+}	
 }
